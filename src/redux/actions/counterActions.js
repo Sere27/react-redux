b2812acc6cfe0f5686a922c954d3fd3a575621a2
Sere27 <vetbat1126@gmail.com>
2020-10-06 17:21:03 +0300

    counter actions added

--- src/redux/actions/counterActions.js
+++ src/redux/actions/counterActions.js
@@ -1,3 +1,19 @@
+import { log } from "console";
 import * as actionTypes from "./actionTypes";
 
-export const
+export const increaseCounter = () => ({
+	type: actionTypes.INCREASE_COUNTER,
+	payload: 1,
+});
+
+export const decreaseCounter = () => ({
+	type: actionTypes.DECREASE_COUNTER,
+	payload: 1,
+});
+
+export const increaseByTwCounter = () => ({
+	type: actionTypes.INCREAS_BY_TO_COUNTER,
+	payload: 2,
+});
+
+//en alt payload 2 olma sebebi 2 2 artacagından
